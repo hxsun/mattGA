b256b3016dacdc8ab94fb376b742be56b68c0d02
Kenneth Sun <sun.moonwell@gmail.com>
2017-01-27 12:58:00 -0500

    initialize project structure

---
+++ src/edu/kelley/excelReader.py
@@ -0,0 +1,17 @@
+'''
+Created on Jan 27, 2017
+
+@author: haoxsun
+'''
+
+class ExcelReader(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
