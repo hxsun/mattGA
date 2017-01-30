1f4c98edd237489d13d4c3dee66df10c30af0a7d
Kenneth Sun <sun.moonwell@gmail.com>
2017-01-30 13:14:50 -0500

    Build the file structure

--- src/edu/kelley/excelReader.py
+++ src/edu/kelley/excelReader.py
@@ -3,6 +3,29 @@ Created on Jan 27, 2017
 
 @author: haoxsun
 '''
+from openpyxl import load_workbook
+
+wb = load_workbook("../../../data/RAIND1.xlsx")
+print(wb.get_sheet_names())
+
+# import os 
+# dir_path = os.path.dirname(os.path.realpath(__file__))
+# print(dir_path)
+
+#from openpyxl.compat import range
+#from openpyxl.utils import get_column_letter
+ws = wb.worksheets[0]
+columns = ["B", "K", "M"]
+column_names = []
+for name in columns:
+    column_names.append(ws[name + "1"].value)
+    
+print(column_names)
+
+
+# wb = Workbook()
+
+# ws = wb.active
 
 class ExcelReader(object):
     '''
