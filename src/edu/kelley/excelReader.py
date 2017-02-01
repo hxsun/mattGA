5522b2539b8219967a95e48e29916dc1bb42dfe7
Kenneth Sun <sun.moonwell@gmail.com>
2017-01-31 23:44:31 -0500

    Finished parsing first excel and storing all the records into mongodb

--- src/edu/kelley/excelReader.py
+++ src/edu/kelley/excelReader.py
@@ -4,37 +4,47 @@ Created on Jan 27, 2017
 @author: haoxsun
 '''
 from openpyxl import load_workbook
-
-wb = load_workbook("../../../data/RAIND1.xlsx")
-print(wb.get_sheet_names())
-
-# import os 
-# dir_path = os.path.dirname(os.path.realpath(__file__))
-# print(dir_path)
-
-#from openpyxl.compat import range
-#from openpyxl.utils import get_column_letter
-ws = wb.worksheets[0]
-columns = ["B", "K", "M"]
-column_names = []
-for name in columns:
-    column_names.append(ws[name + "1"].value)
-    
-print(column_names)
-
-
-# wb = Workbook()
-
-# ws = wb.active
+from openpyxl.utils import coordinate_from_string, column_index_from_string
+from pymongo import MongoClient
 
 class ExcelReader(object):
     '''
     classdocs
     '''
-
-
-    def __init__(self, params):
+    def __init__(self, fileLocation):
         '''
         Constructor
         '''
+        self.fileLocation = fileLocation
         
+    def loadLnkFile(self):
+        # "../../../data/RAIND1.xlsx"
+        wb = load_workbook(self.fileLocation)
+
+        ws = wb.get_sheet_by_name("raind1")
+        columns = ["B", "K", "M"]
+        row_counter = 0
+
+        # Initiate the mongodb client
+        client = MongoClient("localhost", 27017)
+        db = client.gadb
+        linkCollection = db.linkCollection
+        # dataRecords = []
+        
+        for row in ws.iter_rows(row_offset=1):
+
+            row_counter = row_counter + 1
+            if row[column_index_from_string(columns[0])-1].value is not None:
+                dateStr = '{:%Y-%m-%d}'.format(row[column_index_from_string(columns[0])-1].value)
+                CIKStr = row[column_index_from_string(columns[1])-1].value
+                url = row[column_index_from_string(columns[2])-1].value
+                result = linkCollection.update({'Date':dateStr, 'CIK': CIKStr}, 
+                                      {"$set": {'Date':dateStr, 'CIK': CIKStr, 'URL':url}}, upsert = True)
+                print("%s %s %s"%(dateStr, CIKStr, url))
+                
+                #print(ws[columns[0] + row] + " " + ws[columns[1] + row] + " "+ ws[columns[2] + row])
+        print("This sheet has %d rows" % (row_counter))
+        
+
+linkFileReader = ExcelReader( "../../../data/RAIND1.xlsx")
+linkFileReader.loadLnkFile()
